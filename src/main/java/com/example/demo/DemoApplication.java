7d3f670626bec15b3fc8132ed30c7546d716d26a
A124741 <A124741@EDPPC251165.outsourcing.intra.cathaybk>
2018-01-31 15:51:34 +0800

    commit4

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("aaa111");
+		System.out.println("aaa222");
 	}
 }
